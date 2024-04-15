32e099a9ab814bb2b652f13d09c81cbaf1669b6f
Benatge <benat.gerique1@gmail.com>
2024-04-15 20:19:52 +0200

    a

--- php/index.php
+++ php/index.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="\Clase\Holanda\SharedProject\Views\header.css">
-    <link rel="stylesheet" href="\Clase\Holanda\SharedProject\Views\footer.css">
+    <link rel="stylesheet" href="..\Views\header.css">
+    <link rel="stylesheet" href="..\Views\footer.css">
     <script src="https://kit.fontawesome.com/0c3005efe7.js" crossorigin="anonymous"></script>
     <title>ROC-TILBURG</title>
 </head>
