eae3c47ed6492a94b281043fcee6a1dfd3a148be
Benatge <benat.gerique1@gmail.com>
2024-04-15 20:57:39 +0200

    Index terminado

--- Views/header.php
+++ Views/header.php
@@ -1,3 +1,14 @@
+
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#home').click(function() {
+                window.location.href = '../php/index.php';
+            });
+        });
+    </script>
+</head> 
 <div class = "principal">
     <div class="tilburImg">
         <img src="../public/header.png"
@@ -6,7 +17,7 @@
         <div class="rocImg">
             <img src="../public/logo.jpeg"
             width="20%"
-            height="10%">
+            height="10%" id="home">
         </div>
     </div>
 </div>
--- php/index.php
+++ php/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="..\Views\header.css">
     <link rel="stylesheet" href="..\Views\footer.css">
+    <link rel="stylesheet" href="..\Views\index.css">
     <script src="https://kit.fontawesome.com/0c3005efe7.js" crossorigin="anonymous"></script>
     <title>ROC-TILBURG</title>
 </head>
@@ -12,13 +13,27 @@
     <?php
         require_once("../Views/header.php");
     ?>
-    <div name= "about">
-    </div>
-    <div name= "activities">
-    </div>
-    <div name= "point-of-interest">
-    </div>
-    <div name= "project">
+    <div class="container">
+        <div class="row">
+            <div class="imagen">
+                <img src="../public/Tilburgo.jpeg" alt="Imagen 1" class = "foto">
+                <h3>Info about Tilburg</h3>
+            </div>
+            <div class="imagen">
+                <img src="../public/hockey.jpg" alt="Imagen 2" class = "foto">
+                <h3>Common activities</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="imagen">
+                <img src="../public/chapel.jpg" alt="Imagen 3" class = "foto">
+                <h3>Point of interest</h3>
+            </div>
+            <div class="imagen">
+                <img src="../public/project.jpg" alt="Imagen 4" class = "foto">
+                <h3>Out project</h3>
+            </div>
+        </div>
     </div>
     <?php
         require_once("../Views/footer.php");
