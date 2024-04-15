bcc6047b89bab18c7ad5527c1d89ae1299fd4e5f
benatge2 <benat.gerique1@gmail.com>
2024-04-15 10:30:12 +0200

    carpeta sorkuntza

---
+++ php/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ROC-TILBURG</title>
+</head>
+<body>
+    <div name= "about">
+    </div>
+    <div name= "activities">
+    </div>
+    <div name= "point-of-interest">
+    </div>
+    <div name= "project">
+    </div>
+    
+</body>
+</html>
