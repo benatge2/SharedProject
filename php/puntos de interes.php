02803b4e58858ed7e46b66f74feb64dd15adc5d8
AnderIY <andersancheziy@gmail.com>
2024-04-15 11:43:07 +0200

    PuntosInteresAñadidos

---
+++ php/puntos de interes.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Puntos de Interés en Tilburg</title>
+    <link rel="stylesheet" href="PuntosDeInteres.css">
+</head>
+<body>
+    <header>
+        <h1>Puntos de Interés en Tilburg</h1>
+    </header>
+    <main>
+        <section class="interest-section">
+            <div class="interest-item">
+                <img src="Tilburg_kerk.jpg" alt="Castillo de Tilburg">
+                <h2>Castillo de Tilburg</h2>
+                <p>Este castillo medieval es uno de los monumentos más emblemáticos de la ciudad.</p>
+            </div>
+            <div class="interest-item">
+                <img src="MuseoTilburg.jpg" alt="Museo de Tilburg">
+                <h2>Museo de Tilburg</h2>
+                <p>Este museo alberga una colección de arte moderno y contemporáneo, incluyendo obras de artistas como Picasso y Van Gogh.</p>
+            </div>
+        </section>
+        
+    </main>
+</body>
+</html>
